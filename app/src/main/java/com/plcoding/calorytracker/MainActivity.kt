898bdd4ebe27281e7908f4ae769d6b4ff79ccd47
Josh <graeberj88@gmail.com>
2023-09-14 21:40:10 -0700

    setting up module structure, welcome screen, util, navigation, preferences, onboarding, gender screen, age screen

---
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.plcoding.calorytracker
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.ui.Modifier
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.core.navigation.Route
+import com.example.onboarding_presentation.age.AgeScreen
+import com.example.onboarding_presentation.gender.GenderScreen
+import com.example.onboarding_presentation.welcome.WelcomeScreen
+import com.plcoding.calorytracker.navigation.navigate
+import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaloryTrackerTheme {
+                val navController = rememberNavController()
+                val scaffoldState = rememberScaffoldState()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    scaffoldState = scaffoldState
+                ) {
+                    NavHost(
+                        navController = navController,
+                        startDestination = Route.WELCOME
+                    ) {
+                        composable(Route.WELCOME) {
+                            WelcomeScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.AGE) {
+                            AgeScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
+                        }
+                        composable(Route.GENDER) {
+                            GenderScreen(onNavigate = navController::navigate)
+                        }
+                        composable(Route.HEIGHT) {
+
+                        }
+                        composable(Route.WEIGHT) {
+
+                        }
+                        composable(Route.NUTRIENT_GOAL) {
+
+                        }
+                        composable(Route.ACTIVITY) {
+
+                        }
+                        composable(Route.GOAL) {
+
+                        }
+                        composable(Route.TRACKER_OVERVIEW) {
+
+                        }
+                        composable(Route.SEARCH) {
+
+                        }
+                    }
+                }
+
+
+            }
+        }
+    }
+}
---
+++ core/src/main/java/com/example/core/domain/use_case/FilterDigits.kt
@@ -0,0 +1,8 @@
+package com.example.core.domain.use_case
+
+class FilterDigits {
+
+    operator fun invoke(text: String): String {
+        return text.filter { it.isDigit() }
+    }
+}
