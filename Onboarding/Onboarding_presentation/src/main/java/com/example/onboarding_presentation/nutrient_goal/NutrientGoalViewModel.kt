1df25905edfc6663c5be6c7f6019a36773b6eb7a
Josh <graeberj88@gmail.com>
2023-09-14 23:16:20 -0700

    activity level, weight goal, nutrient goal screens and viewmodels

---
+++ Onboarding/Onboarding_presentation/src/main/java/com/example/onboarding_presentation/nutrient_goal/NutrientGoalViewModel.kt
@@ -0,0 +1,67 @@
+package com.example.onboarding_presentation.nutrient_goal
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.core.domain.preferences.Preferences
+import com.example.core.domain.use_case.FilterDigits
+import com.example.core.navigation.Route
+import com.example.core.util.UiEvent
+import com.example.onboarding_domain.use_case.ValidateNutrients
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class NutrientGoalViewModel @Inject constructor(
+    private val preferences: Preferences,
+    private val filterDigits: FilterDigits,
+    private val validateNutrients: ValidateNutrients
+) : ViewModel() {
+
+    var state by mutableStateOf(NutrientGoalState())
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onEvent(event: NutrientGoalEvent) {
+        when(event) {
+            is NutrientGoalEvent.OnCarbRatioEntered -> {
+                state = state.copy(carbsRatio = filterDigits(event.ratio))
+            }
+            is NutrientGoalEvent.OnFatRatioEntered -> {
+                state = state.copy(fatRatio = filterDigits(event.ratio))
+            }
+            is NutrientGoalEvent.OnProteinRatioEntered -> {
+                state = state.copy(proteinRatio = filterDigits(event.ratio))
+            }
+            is NutrientGoalEvent.OnNextClicked -> {
+                val result = validateNutrients(
+                    carbsRatioText = state.carbsRatio,
+                    proteinRatioText = state.proteinRatio,
+                    fatRatioText = state.fatRatio
+                )
+                when(result) {
+                    is ValidateNutrients.Result.Success -> {
+                        preferences.saveCarbRatio(result.carbsRatio)
+                        preferences.saveProteinRatio(result.proteinRatio)
+                        preferences.saveFatRatio(result.fatRatio)
+                        viewModelScope.launch {
+                            _uiEvent.send(UiEvent.Navigate(Route.TRACKER_OVERVIEW))
+                        }
+                    }
+                    is ValidateNutrients.Result.Error -> {
+                        viewModelScope.launch {
+                            _uiEvent.send(UiEvent.ShowSnackbar(result.message))
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
--- app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
+++ app/src/main/java/com/plcoding/calorytracker/MainActivity.kt
@@ -12,10 +12,13 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.core.navigation.Route
+import com.example.onboarding_presentation.activity_level.ActivityLevelScreen
 import com.example.onboarding_presentation.age.AgeScreen
 import com.example.onboarding_presentation.gender.GenderScreen
+import com.example.onboarding_presentation.goal.GoalScreen
 import com.example.onboarding_presentation.height.HeightScreen
 import com.example.onboarding_presentation.height.WeightScreen
+import com.example.onboarding_presentation.nutrient_goal.NutrientGoalScreen
 import com.example.onboarding_presentation.welcome.WelcomeScreen
 import com.plcoding.calorytracker.navigation.navigate
 import com.plcoding.calorytracker.ui.theme.CaloryTrackerTheme
@@ -63,13 +66,20 @@ class MainActivity : ComponentActivity() {
                             )
                         }
                         composable(Route.NUTRIENT_GOAL) {
-
+                            NutrientGoalScreen(
+                                scaffoldState = scaffoldState,
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.ACTIVITY) {
-
+                            ActivityLevelScreen(
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.GOAL) {
-
+                            GoalScreen(
+                                onNavigate = navController::navigate
+                            )
                         }
                         composable(Route.TRACKER_OVERVIEW) {
 
