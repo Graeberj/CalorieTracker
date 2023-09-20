be794c7f14106f2ce93655fee2481e8de20d679c
Josh <graeberj88@gmail.com>
2023-09-19 18:32:22 -0700

    Day selector, expandable meal UI, tracked food item Ui

--- Tracker/Tracker_presentation/src/main/java/com/example/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ Tracker/Tracker_presentation/src/main/java/com/example/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -1,15 +1,25 @@
 package com.example.tracker_presentation.tracker_overview
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.core.util.UiEvent
 import com.example.core_ui.LocalSpacing
+import com.example.tracker_presentation.tracker_overview.components.AddButton
+import com.example.tracker_presentation.tracker_overview.components.DaySelector
 import com.example.tracker_presentation.tracker_overview.components.NutrientHeader
+import com.example.tracker_presentation.tracker_overview.components.TrackedFoodItem
+import com.plcoding.core.R
 
 @Composable
 fun TrackerOverviewScreen(
@@ -19,14 +29,68 @@ fun TrackerOverviewScreen(
     val spacing = LocalSpacing.current
     val state = viewModel.state
     val context = LocalContext.current
-    
+
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
             .padding(bottom = spacing.spaceMedium)
-    ){
+    ) {
         item {
             NutrientHeader(state = state)
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+            DaySelector(
+                date = state.date,
+                onPreviousDayClicked = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClicked)
+                },
+                onNextDayClicked = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnNextDayClicked)
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = spacing.spaceMedium)
+            )
+            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+        }
+        items(state.meals) { meal ->
+            ExpandableMeal(
+                meal = meal,
+                onToggleClick = {
+                    viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClicked(meal))
+                },
+                content = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+                        state.trackedFood.forEach { food ->
+                            TrackedFoodItem(
+                                trackedFood = food,
+                                onDeleteClicked = {
+                                    viewModel.onEvent(
+                                        TrackerOverviewEvent.OnDeleteTrackedFoodClicked(food)
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                        }
+                        AddButton(
+                            text = stringResource(
+                                id = R.string.add_meal,
+                                meal.name.asString(context)
+                            ),
+                            onClick = {
+                                viewModel.onEvent(
+                                    TrackerOverviewEvent.OnAddFoodClicked(meal)
+                                )
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
+                },
+                modifier = Modifier.fillMaxWidth()
+            )
         }
     }
 }
